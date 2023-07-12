d376279954fc1b3d2fb28d3ac20085f5e922cc9a
BalaChellammaV <121444048+BalaChellammaV@users.noreply.github.com>
2023-07-12 19:17:45 +0530

    functionality completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import emojiData from './emojiData.json' 
+//this json file contains an array of objects which holds a data about "emoji"  
 
 function App() {
+
+  //to store and update value in the search bar
+  const [search,setSearch] = useState('');
+  //to store and update the value of array of <h1></h1> tag
+  const [data, setData] = useState([]);
+
+  /**
+   * useEffect() => this hook is used here to update the state value whenever any changes in the search bar.
+   * 
+   * using "filter() , filter the data of the emoji which matches the data in the search bar"
+   * store the value which returns by "filter() into "emojiDataVal"
+   * and then pass the "emojiDataVal" in setData() to update the data (state variable)
+   * 
+   */
+
+  useEffect(()=>{
+     const emojiDataVal = emojiData.filter((emoji)=>{
+      return emoji.title.toLowerCase().includes(search.toLocaleLowerCase())});
+     setData(emojiDataVal);
+     
+  },[search]);
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+       <div className='input_container'>
+         <input type="text" 
+         className='search_input' 
+         value={search} 
+         onChange={(e)=>{
+            setSearch(e.target.value);
+         }}/>
+       </div>
+       <div className='emoji_container'>
+          {data.map((emoji)=>{
+            return(
+            <h1 key={emoji.title}>
+              {emoji.title} {emoji.symbol}
+            </h1>
+            )
+          })}
+       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
